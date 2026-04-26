293d8805aa8668a838b5f12b3b7771f54dc886e1
Kanan <kanankotwani28@gmail.com>
2026-04-27 00:04:37 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.39%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int memo(int dp[] , int n){
+        if(n<=1) return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = memo(dp,n-1)+ memo(dp,n-2);
+    }
+    public int climbStairs(int n) {
+        int dp[]= new int[n+1];
+        Arrays.fill(dp,-1);
+        return dp[n] = memo(dp,n);
+    }
+}
