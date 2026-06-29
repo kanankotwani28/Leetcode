2df0e9125832baa4f9c8edb6e7b2249cfc6b22f1
Kanan <kanankotwani28@gmail.com>
2026-06-29 08:29:52 +0530

    Time: 23 ms (65.6%), Space: 52.1 MB (56.75%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,28 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(nums[i]);
+            } else {
+                if (nums[i] > pq.peek()) {
+                    pq.poll();
+                    pq.offer(nums[i]);
+                }
+            }
+        }
+    }
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.offer(val);
+        } else if (val > pq.peek()) {
+            pq.poll();
+            pq.offer(val);
+        }
+
+        return pq.peek();
+    }
+}
