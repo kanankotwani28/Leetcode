cac130d82f599ada90cd17135566d22e98ac5f19
Kanan <kanankotwani28@gmail.com>
2026-07-12 02:15:10 +0530

    Time: 19 ms (71.87%), Space: 54.4 MB (41.3%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDistinct(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][]dp = new int[n+1][m+1];
+        for(int i = 0 ; i<=n ; i++) dp[i][0] = 1;
+        for(int j = 1 ; j<=m ; j++) dp[0][j] = 0;
+
+        for(int ind1 = 1 ; ind1 <=n ; ind1 ++){
+            for(int ind2 = 1 ; ind2 <=m ; ind2 ++){
+                if(s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1)){
+                    dp[ind1][ind2] = dp[ind1 - 1][ind2 - 1] + dp[ind1 -1][ind2];
+                }
+                else{
+                    dp[ind1][ind2] = dp[ind1-1][ind2];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
