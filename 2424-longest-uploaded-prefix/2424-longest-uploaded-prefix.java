7adcfdbe7daf9ebcbbbfbb5129cff375ffd9a160
Kanan <kanankotwani28@gmail.com>
2026-08-12 17:26:43 +0530

    Time: 59 ms (14.62%), Space: 132.3 MB (91.54%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -1,19 +1,41 @@
 class LUPrefix {
-    HashSet <Integer> uploaded;
-    int next ;
+    int []tree;
+    int n;
     public LUPrefix(int n) {
-        uploaded = new HashSet<>();
-        next = 1;
+        this.n = n ;
+        tree = new int[4*n];
     }
     
     public void upload(int video) {
-        uploaded.add(video);
-        while(uploaded.contains(next))
-            next++;
+        update(1,1,n,video);
     }
     
     public int longest() {
-        return next-1;
+       return tree[1];
+    }
+
+    private void update(int node , int start , int end , int video){
+        if(start == end){
+            tree[node] = 1;
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        if(video<=mid ) update(2*node , start , mid ,video);
+        else update(2*node + 1 , mid+1 , end , video);
+
+        int leftChild = node * 2 ;
+        int rightChild = node * 2 + 1;
+
+        int length = mid - start + 1;
+        if(tree[leftChild] == length){
+            tree[node] = length + tree[rightChild];
+        }
+        else {
+            tree[node] = tree[leftChild];
+        }
+
+
     }
 }
 
