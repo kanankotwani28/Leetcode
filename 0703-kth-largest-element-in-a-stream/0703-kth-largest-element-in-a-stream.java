3f448b7b60a1d4d0121b77b424f718d26264ba54
Kanan <kanankotwani28@gmail.com>
2026-08-20 18:29:13 +0530

    Time: 22 ms (84.18%), Space: 52.6 MB (6.18%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,28 +1,41 @@
 class KthLargest {
-    PriorityQueue<Integer> pq;
     int k;
+    PriorityQueue<Integer> pq;
     public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>(); // min heap at default
         this.k = k;
-        pq = new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (pq.size() < k) {
-                pq.offer(nums[i]);
-            } else {
-                if (nums[i] > pq.peek()) {
+        int n = nums.length;
+        for(int i = 0; i < n ; i++){
+            if(pq.size() < k){
+                pq.add(nums[i]);
+            }
+            else{
+                if(pq.peek() < nums[i]){
                     pq.poll();
-                    pq.offer(nums[i]);
+                    pq.add(nums[i]);
                 }
             }
         }
     }
+    
     public int add(int val) {
-        if (pq.size() < k) {
-            pq.offer(val);
-        } else if (val > pq.peek()) {
-            pq.poll();
-            pq.offer(val);
+        if(pq.size() < k){
+            pq.add(val);
+        }
+
+        else{
+            if(pq.peek() < val){
+                pq.poll();
+                pq.add(val);
+            }
         }
 
         return pq.peek();
     }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
