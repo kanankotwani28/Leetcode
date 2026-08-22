73546499ef3f76aed97a67a1ed976f345bc9ff38
Kanan <kanankotwani28@gmail.com>
2026-08-22 16:47:25 +0530

    Time: 46 ms (8.77%), Space: 48.1 MB (78.13%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,21 +1,73 @@
 class Solution {
-    public int leastInterval(char[] tasks, int n) {
-        int[] freq = new int[26];
-        for (char task : tasks)
-            freq[task - 'A']++;
+    class Pair{
+        int ch;
+        int freq;
+        Pair(int ch , int freq){
+            this.ch = ch ;
+            this.freq = freq;
+        }
+    }
 
-        Arrays.sort(freq);
-        int maxFreq = freq[25];
-        int maxCount = 0;
-        for (int i = 25; i >= 0; i--) {
-            if (freq[i] == maxFreq) maxCount++;
-            else break;
+    class Cooldown{
+        int ch;
+        int freq;
+        int schtime;
+        Cooldown(int ch , int freq , int schtime){
+            this.ch = ch;
+            this.freq = freq;
+            this.schtime = schtime;
+        }
+    }
+
+    public int leastInterval(char[] tasks, int n) {
+        int []freq = new int[26];
+        for(int i = 0 ; i < tasks.length ; i++){
+            freq[tasks[i] - 'A']++;
         }
 
-        int partCount = maxFreq - 1;
-        int partLength = n + 1;
-        int emptySlots = partCount * partLength + maxCount;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq - a.freq);
+        for(int i = 0; i< 26 ; i++){
+            if(freq[i] > 0) pq.add(new Pair(i,freq[i]));
+        }
 
-        return Math.max(tasks.length, emptySlots);
+        Queue<Cooldown> q = new LinkedList<>();
+
+        int time = 0;
+        
+        while(!pq.isEmpty() || !q.isEmpty()){
+
+            while(!q.isEmpty() &&
+                    q.peek().schtime <= time){
+                        Cooldown c = q.poll();
+pq.add(new Pair(c.ch, c.freq));
+            }
+
+            if(!pq.isEmpty()){
+                Pair p = pq.poll();
+                time++;
+                p.freq--;
+                if (p.freq > 0) {
+                    q.add(new Cooldown(
+                        p.ch,
+                        p.freq,
+                        time + n
+                    ));
+                }
+            }
+
+            else{
+                time++;
+            }
+        }
+
+    return time;
     }
 }
+
+// lets make a count of each of the character along with the frweq
+// the one with the highets freq will be arrangeed first atleast at an intervela of n 
+// the updated inyterval is then added into the heap 
+// return the length of the ans arry
+
+// freq i supdated 
+// not the last one 
